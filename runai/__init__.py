619fdf34bb4ed5812d3705912a4c976d129eef16
levosos <razhaleva@gmail.com>
2019-10-07 16:24:03 +0300

    MP-supported 'Dense' Keras layer
    Introducing runai.mp subpackage
    
    - supporting both c-in and c-out methods
    - implementation based on Keras as of today (master@8904340)

--- runai/__init__.py
+++ runai/__init__.py
@@ -1,3 +1,5 @@
 from .utils import log # must be first for all submodules to import
+
 from . import flex
 from . import ga
+from . import mp
---
+++ runai/mp/__init__.py
@@ -0,0 +1,7 @@
+import enum
+
+class Method(enum.Enum):
+    Cin  = 0
+    Cout = 1
+
+from . import keras
