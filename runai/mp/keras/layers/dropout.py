809820a599516a26cbb16a4f3f6d332a6208134b
levosos <razhaleva@gmail.com>
2019-10-16 10:31:26 +0300

    Added generic MP-supported 'Keep' layer implementation
    - both 'Dropout' and 'MaxPooling2D' layers inherit it
    - added UTs

--- runai/mp/keras/layers/dropout.py
+++ runai/mp/keras/layers/dropout.py
@@ -1,21 +1,5 @@
 import keras.layers
 
-from runai import log
+from .keep import Keep
 
-from . import coordinator
-
-class Dropout(keras.layers.Dropout):
-    def call(self, inputs):
-        assert not isinstance(inputs, (tuple, list))
-
-        if not coordinator.registered(inputs):
-            log.debug('Not parallelising dropout layer \'%s\'', self.name)
-            return super(Dropout, self).call(inputs)
-
-        log.info('Using parallelised input for dropout layer \'%s\'', self.name)
-
-        inputs = coordinator.resolve(inputs)
-        outputs = [super(Dropout, self).call(input) for input in inputs]
-        merged = keras.layers.Concatenate(axis=-1)(outputs)
-        coordinator.register(merged, outputs)
-        return merged
+class Dropout(Keep, keras.layers.Dropout): pass
