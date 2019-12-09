d4f6cf21a640cfcf46a4fef4e0b102ea5f118525
levosos <razhaleva@gmail.com>
2019-12-09 17:24:10 +0200

    Using 'nvidia-smi' to retrieve the # of available GPUs

--- runai/utils/gpus.py
+++ runai/utils/gpus.py
@@ -1,16 +1,14 @@
-import os
-
 # NOTE(levosos): we do not use TensorFlow's API (tensorflow.python.client.device_lib.list_local_devices())
 # because it causes all devices to be mapped to the current process and when using Horovod
 # we receive the next error: "TensorFlow device (GPU:0) is being mapped to multiple CUDA devices".
 
-DIR = "/proc/driver/nvidia/gpus"
+import subprocess
 
 def available():
-    return os.path.isdir(DIR)
+    return count() > 0
 
 def count():
-    if not available():
+    try:
+        return int(subprocess.check_output('nvidia-smi --list-gpus | wc -l', shell=True))
+    except subprocess.CalledProcessError:
         return 0
-    
-    return len(os.listdir(DIR))
