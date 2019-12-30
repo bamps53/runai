289b843ae78ef95c7987ce7d4712c99945a2037e
levosos <razhaleva@gmail.com>
2019-12-30 13:54:37 +0200

    Updated version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="runai",
-    version="0.1.1",
+    version="0.1.2",
     author="Run:AI",
     author_email="pypi@run.ai",
     description="Run:AI Python library",
